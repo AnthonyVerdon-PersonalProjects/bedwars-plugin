39d279a7ce07aacf6f1c8bbdd0b586b36cc718cc
averdon <anthonyverdon10@gmail.com>
2023-10-19 17:57:44 +0200

    right click while holding a book event implemented

--- src/fr/nordev/bedwars/MyListener.java
+++ src/fr/nordev/bedwars/MyListener.java
@@ -1,10 +1,17 @@
 package fr.nordev.bedwars;
 
 import org.bukkit.GameMode;
+import org.bukkit.Material;
+import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerChangedWorldEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import net.md_5.bungee.api.ChatColor;
@@ -43,8 +50,34 @@ public class MyListener implements Listener {
 	            public void run(){
 	                main.respawnPlayer(player);
 	            }
-	        }.runTaskLater(main, 20L * 5);
+	        }.runTaskLater(main, 20L * 5); //1L = 1 tick, 20L = 1 sec
 			System.out.println(player.getDisplayName() + " died");
 		}
 	}
+	
+	@EventHandler
+    public void onRightClick (PlayerInteractEvent event) {
+        //Player player = event.getPlayer();
+        if (event.getAction() == Action.RIGHT_CLICK_AIR) {
+            if (event.getItem() != null && event.getItem().getType() == Material.BOOK) {
+                System.out.println("right click while holding book");
+            }
+        }
+    }
+	
+	@EventHandler
+    public void onChangeWorld (PlayerChangedWorldEvent event) {
+        Player player = event.getPlayer();
+        World world = player.getLocation().getWorld();
+        player.getInventory().clear();
+        System.out.println(player.getName() + " has changed of world");
+        if (world.getName() == "lobby") {
+        	ItemStack customBook = new ItemStack(Material.BOOK, 1);
+        	ItemMeta customMeta = customBook.getItemMeta();
+        	customMeta.setDisplayName("Menu");
+        	customBook.setItemMeta(customMeta);
+        	player.getInventory().addItem(customBook);
+        	player.updateInventory();
+        }
+    }
 }
